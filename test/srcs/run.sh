3cb7e60bcf943bd71269972ce6f1837793410f37
Yuri Lee <ylee@c3r1s2.42seoul.kr>
2021-03-22 01:03:13 +0900

    test

---
+++ test/srcs/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update
+service mysql start
+mysql -e "create database wordpress;\
+	create user 'ylee'@'localhost' identified by 'password';\
+	grant all privileges on *.* to 'ylee'@'localhost' with grant option;\
+	flush privileges;"
+service nginx start
+service mysql start
+service php7.3-fpm start
+
+bash
