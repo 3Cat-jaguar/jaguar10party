778ecf80a643a7c32e109f8fb6272242e69d2249
ylee <ryusa13@gmail.com>
2021-03-22 02:18:55 +0900

    modify sh

--- test/srcs/run.sh
+++ test/srcs/run.sh
@@ -3,11 +3,11 @@
 apt update
 service mysql start
 mysql -e "create database if not exists wordpress;\
-	create user 'ylee'@'localhost' identified by 'password';\
+	create user if not exists 'ylee'@'localhost' identified by 'password';\
 	grant all privileges on *.* to 'ylee'@'localhost' with grant option;\
 	flush privileges;"
 service nginx start
-service mysql start
+service mysql reload
 service php7.3-fpm start
 
 bash
